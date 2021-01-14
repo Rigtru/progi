4714caccc6b70d06fce69d79281b662bd98baa36
Rigt <andr614@yandex.ru>
2021-01-14 13:38:51 +0500

    Caculator

--- src/Start.java
+++ src/Start.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class Start {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int result = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Write task");
+        String task = in.nextLine();
+        int plus = task.indexOf('+');
+        int minus = task.indexOf('-');
+        int mul = task.indexOf('*');
+        int div = task.indexOf('/');
+        if (plus >= 0) {
+            String[] primer = task.split("\\+");
+            int a = Integer.parseInt(primer[0]);
+            int b = Integer.parseInt(primer[1]);
+            result = a + b;
+
+
+        } else if (minus >= 0) {
+            String[] primer = task.split("-");
+            int a = Integer.parseInt(primer[0]);
+            int b = Integer.parseInt(primer[1]);
+            result = a - b;
+
+
+        } else if (mul >= 0) {
+            String[] primer = task.split("\\*");
+            int a = Integer.parseInt(primer[0]);
+            int b = Integer.parseInt(primer[1]);
+            result = a * b;
+
+
+        } else if (div >= 0) {
+            String[] primer = task.split("/");
+            int a = Integer.parseInt(primer[0]);
+            int b = Integer.parseInt(primer[1]);
+            result = a / b;
+
+        }
+        System.out.println(result);
     }
 }
